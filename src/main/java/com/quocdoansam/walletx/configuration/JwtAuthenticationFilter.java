c830ace58a462ac0b11000987690ca51e68d12d0
quocdoansam <quocdoansam.me@gmail.com>
2025-04-09 20:36:41 +0700

    Refactor JwtAuthenticationFilter to improve token extraction and authentication handling

--- src/main/java/com/quocdoansam/walletx/configuration/JwtAuthenticationFilter.java
+++ src/main/java/com/quocdoansam/walletx/configuration/JwtAuthenticationFilter.java
@@ -8,7 +8,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -21,29 +21,49 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Autowired
-    private JwtDecoder jwtDecoder;
+    private final JwtDecoder jwtDecoder;
+    private final JwtAuthenticationConverter jwtAuthenticationConverter;
+
+    public JwtAuthenticationFilter(JwtDecoder jwtDecoder, JwtAuthenticationConverter jwtAuthenticationConverter) {
+        this.jwtDecoder = jwtDecoder;
+        this.jwtAuthenticationConverter = jwtAuthenticationConverter;
+    }
 
     @Override
-    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain)
             throws ServletException, IOException {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("access_token")) {
-                    String token = cookie.getValue();
-                    try {
-                        Jwt jwt = jwtDecoder.decode(token);
-                        Authentication authentication = new JwtAuthenticationToken(jwt);
-                        SecurityContextHolder.getContext().setAuthentication(authentication);
-                    } catch (Exception e) {
-                        SecurityContextHolder.clearContext();
-                        return;
-                    }
-                }
+
+        String token = extractTokenFromCookie(request);
+        System.out.println("Token from Cookie: " + token);
+
+        if (token != null) {
+            try {
+                Jwt jwt = jwtDecoder.decode(token);
+                // Authentication authentication = new JwtAuthenticationToken(jwt);
+                Authentication authentication = jwtAuthenticationConverter.convert(jwt);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                System.out.println("Authentication set: " + authentication);
+            } catch (Exception e) {
+                System.out.println("JWT Error: " + e.getMessage());
+                SecurityContextHolder.clearContext();
+                return;
             }
         }
         filterChain.doFilter(request, response);
+    }
+
+    private String extractTokenFromCookie(HttpServletRequest request) {
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if ("access_token".equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
     }
 
 }
