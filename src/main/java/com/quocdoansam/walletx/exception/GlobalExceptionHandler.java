1cc74069c19b491014bdde83b148e9914dc563cf
quocdoansam <quocdoansam.me@gmail.com>
2025-04-09 20:40:11 +0700

    Refactor GlobalExceptionHandler to standardize exception handling and improve response structure

--- src/main/java/com/quocdoansam/walletx/exception/GlobalExceptionHandler.java
+++ src/main/java/com/quocdoansam/walletx/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.quocdoansam.walletx.exception;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.quocdoansam.walletx.dto.response.ApiResponse;
+import com.quocdoansam.walletx.dto.response.BaseResponse;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,18 +19,18 @@ import lombok.extern.slf4j.Slf4j;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = Exception.class)
-    ResponseEntity<ApiResponse<Object>> handlingRuntimeException(RuntimeException exception) {
-        log.error("Unexpected error occurred: ", exception);
-
-        ApiResponse<Object> apiResponse = new ApiResponse<>();
-        apiResponse.setCode(ErrorCode.UNCATEGORIZED_EXCEPTION.getCode());
-        apiResponse.setMessage(ErrorCode.UNCATEGORIZED_EXCEPTION.getMessage());
-
-        return ResponseEntity.badRequest().body(apiResponse);
+    ResponseEntity<BaseResponse<Object>> handleGenericException(Exception exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(BaseResponse.builder()
+                        .success(false)
+                        .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                        .message("An unexpected error occurred.")
+                        .data(null).build());
     }
 
     @ExceptionHandler(value = AppException.class)
-    ResponseEntity<ApiResponse<Object>> handlingAppException(AppException exception) {
+    ResponseEntity<ApiResponse<Object>> handleAppException(AppException exception) {
         log.error("AppException occurred: {}", exception.getMessage(), exception);
 
         ErrorCode errorCode = exception.getErrorCode();
@@ -35,6 +39,17 @@ public class GlobalExceptionHandler {
         apiResponse.setMessage(errorCode.getMessage());
 
         return ResponseEntity.badRequest().body(apiResponse);
+    }
+
+    @ExceptionHandler(value = BaseException.class)
+    ResponseEntity<BaseResponse<Object>> handlingBaseException(BaseException exception) {
+        return ResponseEntity.status(exception.getStatus()).body(
+                BaseResponse.builder()
+                        .success(false)
+                        .statusCode(exception.getStatus().value())
+                        .message(exception.getMessage())
+                        .data(null)
+                        .build());
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
